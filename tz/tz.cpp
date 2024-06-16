5f4c4ff2c7d07c0a4ca05718043198519c8f5874
Wingolym <wingolymqunk@gmail.com>
2024-06-16 22:21:05 +0300

    sdlfk;jhgljkdafhgadlfjkgsoiad;flkgjhdfkjlcvxmbcvx,mnbsdkholagttjkasgt

---
+++ tz/tz.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+
+{
+	int a, b, c;
+	cout << "Enter an integer:";
+	cin >> a;
+	cout << "Enter another integer:" ;
+	cin >> b;
+	c = a + b;
+	cout << "a + b = "<< c << endl;
+	c = a - b;
+	cout << "a - b = " << c;
+}
