501be61d2dace2465cb2112df56d9bb443fbbcd9
Wingolym <wingolymqunk@gmail.com>
2024-06-20 16:26:19 +0300

    tz2

--- tz/tz.cpp
+++ tz/tz.cpp
@@ -7,7 +7,7 @@ int main()
 	int a, b, c;
 	cout << "Enter an integer:";
 	cin >> a;
-	cout << "Enter another integer:" ;
+	cout << "Enter another integer:";
 	cin >> b;
 	c = a + b;
 	cout << "a + b = "<< c << endl;
