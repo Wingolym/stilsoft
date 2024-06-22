32de69d739895cd13c915bb6751b258241e5ea5a
Wingolym <wingolymqunk@gmail.com>
2024-06-22 23:43:55 +0300

    tz4

---
+++ tz4-10/tz4-10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+	unsigned int readNumber()
+{
+	cout << "Enter a positive integer: ";
+	unsigned int input;
+	cin >> input;
+
+	return input;
+}
+
+int main()
+{
+	unsigned int x( readNumber());
+	if (x == 1 or x == 2 or x == 3 or x == 5 or x == 7)
+		cout << "The digit is prime\n";
+	else if ( x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0)
+		cout << "The digit is not prime\n";
+	else
+		cout << "The digit is prime\n";
+
+	
+}
+
---
+++ tz4/tz4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double readNumber()
+{
+	std::cout << "Enter a double value: ";
+	double input{};
+	std::cin >> input;
+
+	return input;
+}
+
+int main()
+{
+	double a(readNumber());
+	double b(readNumber());
+	char let;
+	cout << "Enter one of the following : +, -, *, or / : ";
+	cin >> let;
+	if (static_cast<int>(let) == 43)
+		cout << a << let << b << " = " << a + b;
+	else if (static_cast<int>(let) == 45)
+		cout << a << let << b << " = " << a - b;
+	else if (static_cast<int>(let) == 42)
+		cout << a << let << b << " = " << a * b;
+	else if (static_cast<int>(let) == 47)
+		cout << a << let << b << " = " << a / b;
+	else
+		cout << "This symbol is not a valid mathematical operator.";
+}
